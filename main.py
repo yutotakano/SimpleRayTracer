79f95c5416e13513a52eccb4125980462a3427c8
Yuto Takano <moa17stock@gmail.com>
2020-11-12 20:13:13 +0000

    Change lighting based on t distance

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ class Screen:
                     if len(intersections) == 0:
                         continue
                     closest_intersection = min(intersections, key=(lambda item: item[1]))
-                    value += (80*math.acos((closest_intersection[2].dot(ray.direction)) / (closest_intersection[2].modulo()*ray.direction.modulo())))
+                    value += (80*math.acos((closest_intersection[2].dot(ray.direction)) / (closest_intersection[2].modulo()*ray.direction.modulo()))) / (closest_intersection[1] / 50)
 
                 pixels[i].append(value/4.0)
         return pixels
